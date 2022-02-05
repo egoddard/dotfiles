164ba361914bffa5e1d70e9fb040ff5fa90e742b
Eric Goddard <eric@stratasan.com>
2022-02-05 12:36:12 -0600

    add starship config export to zshrc

--- devcontainer.sh
+++ devcontainer.sh
@@ -1,7 +1,2 @@
-sudo apt install -y tmux fonts-firacode
-
-cd $HOME
-git clone https://github.com/gpakosz/.tmux.git
-ln -s $HOME/dotfiles/tmux.conf.local $HOME/.tmux.conf.local
-
 echo "alias gb='git branch --sort=-committerdate'" >> ~/.zshrc
+echo "export STARSHIP_CONFIG=$HOME/dotfiles/starship.toml" >> ~/.zshrc
