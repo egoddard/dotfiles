41f134c9bfef52ae4c76f507dd1352c7ee1b765c
Eric Goddard <eric@stratasan.com>
2022-02-05 20:53:53 -0600

    Set tzinfo in container

--- devcontainer.sh
+++ devcontainer.sh
@@ -2,3 +2,5 @@ echo "bindkey -v" >> ~/.zshrc
 echo "alias gb='git branch --sort=-committerdate'" >> ~/.zshrc
 echo "export STARSHIP_CONFIG=$HOME/dotfiles/starship.toml" >> ~/.zshrc
 
+sudo unlink /etc/localtime
+sudo ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime
