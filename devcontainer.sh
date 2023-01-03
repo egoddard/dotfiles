e42c69ec801b512b16211cf13d476d88cb7f4bbd
Eric Goddard <eric@stratasan.com>
2023-01-03 09:31:21 -0600

    Install 1password in devcontainer.sh

--- devcontainer.sh
+++ devcontainer.sh
@@ -4,3 +4,8 @@ echo "export STARSHIP_CONFIG=$HOME/dotfiles/starship.toml" >> ~/.zshrc
 
 sudo unlink /etc/localtime
 sudo ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime
+
+curl -sSfo op.zip \
+  https://cache.agilebits.com/dist/1P/op2/pkg/v2.12.0/op_linux_arm64_v2.12.0.zip \
+  && sudo unzip -od /usr/local/bin/ op.zip \
+  && rm op.zip
