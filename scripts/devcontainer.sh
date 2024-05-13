5248ac60be1730c8aa129b3ae015269b5630ee1f
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-05-13 15:40:57 -0500

    Update 1password version in devcontainer.sh

--- scripts/devcontainer.sh
+++ scripts/devcontainer.sh
@@ -1,17 +1,17 @@
-echo "bindkey -v" >> ~/.zshrc
-echo "set -o vi" >> ~/.bashrc
+echo "bindkey -v" >>~/.zshrc
+echo "set -o vi" >>~/.bashrc
 
-echo "alias gb='git branch --sort=-committerdate'" >> ~/.zshrc
-echo "alias gb='git branch --sort=-committerdate'" >> ~/.bashrc
-echo "alias tree='tree --dirsfirst -I __pycache__ -I node_modules'" >> ~/.zshrc
+echo "alias gb='git branch --sort=-committerdate'" >>~/.zshrc
+echo "alias gb='git branch --sort=-committerdate'" >>~/.bashrc
+echo "alias tree='tree --dirsfirst -I __pycache__ -I node_modules'" >>~/.zshrc
 
-echo "export STARSHIP_CONFIG=$HOME/dotfiles/starship.toml" >> ~/.zshrc
-echo "export STARSHIP_CONFIG=$HOME/dotfiles/starship.toml" >> ~/.bashrc
+echo "export STARSHIP_CONFIG=$HOME/dotfiles/starship.toml" >>~/.zshrc
+echo "export STARSHIP_CONFIG=$HOME/dotfiles/starship.toml" >>~/.bashrc
 
 sudo unlink /etc/localtime
 sudo ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime
 
 curl -sSfo op.zip \
-  https://cache.agilebits.com/dist/1P/op2/pkg/v2.12.0/op_linux_arm64_v2.12.0.zip \
-  && sudo unzip -od /usr/local/bin/ op.zip \
-  && rm op.zip
+	https://cache.agilebits.com/dist/1P/op2/pkg/v2.27.0/op_linux_arm64_v2.27.0.zip &&
+	sudo unzip -od /usr/local/bin/ op.zip &&
+	rm op.zip
