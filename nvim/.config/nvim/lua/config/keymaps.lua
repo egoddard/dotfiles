fbed93e048fb5bf1522d45d0bcd4fe1ab95bba4b
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-05-15 18:23:30 -0500

    Update nvim keymaps and options

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local set = vim.keymap.set
+
+set("n", "<M-,>", "<c-w>2<")
+set("n", "<M-.>", "<c-w>2>")
+set("n", "<M-t>", "<c-w>2+")
+set("n", "<M-s>", "<c-w>2-")
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.python3_host_prog = "$HOME/.virtualenvs/pynvim/bin/python"
+vim.opt.inccommand = "nosplit"
+vim.opt.relativenumber = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
