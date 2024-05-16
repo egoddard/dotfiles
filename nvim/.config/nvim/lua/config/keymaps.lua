e5cb6fcc9b8ad5a4b4dabf23f8ba660177f01c07
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-05-16 14:34:31 -0500

    Add keymaps for terminal and resizing buffers

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,25 @@ set("n", "<M-,>", "<c-w>2<")
 set("n", "<M-.>", "<c-w>2>")
 set("n", "<M-t>", "<c-w>2+")
 set("n", "<M-s>", "<c-w>2-")
+
+-- Set local settings for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.scrolloff = 0
+  end,
+})
+
+-- Easily hit escape in terminal mode.
+set("t", "<esc><esc>", "<c-\\><c-n>")
+
+-- Open a terminal at the bottom of the screen with a fixed height
+set("n", ",st", function()
+  vim.cmd.new()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
+end)
