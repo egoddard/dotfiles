8f93968f0130be4c2d6b5a0c37a9ce554d3d13ee
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-05-25 08:52:18 -0500

    Customize wezterm colors and have it adjust based on system theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,9 +1,27 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Dark Macchiato'
+  else
+    return 'Catppuccin Latte'
+  end
+end
+
+local custom_catppuccin_mocha = wezterm.color.get_builtin_schemes()['Catppuccin Mocha']
+custom_catppuccin_mocha.background = '#11111b'
+
+local custom_catppuccin_macchiato = wezterm.color.get_builtin_schemes()['Catppuccin Macchiato']
+custom_catppuccin_macchiato.background = '#151515'
 
 local config = wezterm.config_builder()
+config.color_schemes = {
+  ['Catppuccin Dark Mocha'] = custom_catppuccin_mocha,
+  ['Catppuccin Dark Macchiato'] = custom_catppuccin_macchiato,
+}
 
-config.font = wezterm.font("Fira Code")
+config.font = wezterm.font 'Fira Code'
 config.font_size = 13
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
 return config
