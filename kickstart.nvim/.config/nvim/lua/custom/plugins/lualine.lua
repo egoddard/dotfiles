50ea362a71543d9d0c0196b9417d2763137e765c
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-06-05 21:39:26 -0500

    Add copilot status to lualine

--- kickstart.nvim/.config/nvim/lua/custom/plugins/lualine.lua
+++ kickstart.nvim/.config/nvim/lua/custom/plugins/lualine.lua
@@ -1,7 +1,13 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'AndreM222/copilot-lualine',
+  },
   opts = {
     theme = 'catppuccin',
+    sections = {
+      lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
+    },
   },
 }
