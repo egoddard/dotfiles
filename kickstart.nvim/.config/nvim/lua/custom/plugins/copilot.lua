67fe434a586a27c74c5038f60dfa1af41d843205
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-06-05 21:39:13 -0500

    Add filetypes config to copilot

--- kickstart.nvim/.config/nvim/lua/custom/plugins/copilot.lua
+++ kickstart.nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -7,6 +7,20 @@ return {
       require('copilot').setup {
         suggestion = { enabled = false },
         panel = { enabled = false },
+        filetypes = {
+          markdown = true,
+          help = true,
+          text = false,
+          sh = function()
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+              return false
+            end
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.envrc.*') then
+              return false
+            end
+            return true
+          end,
+        },
       }
     end,
   },
