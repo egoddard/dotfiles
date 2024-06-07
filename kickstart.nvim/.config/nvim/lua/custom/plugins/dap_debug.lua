622b126ab24744f4577a07bc1e2d3452f2463b74
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-06-07 08:27:25 -0500

    Add dapui toggle keymap

--- kickstart.nvim/.config/nvim/lua/custom/plugins/dap_debug.lua
+++ kickstart.nvim/.config/nvim/lua/custom/plugins/dap_debug.lua
@@ -59,6 +59,7 @@ return {
     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
     vim.keymap.set('n', '<F4>', dap.step_back, { desc = 'Debug: Step Back' })
+    vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = 'Dap UI' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
