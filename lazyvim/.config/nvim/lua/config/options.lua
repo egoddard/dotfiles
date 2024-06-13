aee7173ad8072497430d5367d2499da2ec3d8120
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2024-06-13 18:07:05 -0500

    Set root_spec and fix python3 path

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -1,8 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.python3_host_prog = "$HOME/.virtualenvs/pynvim/bin/python"
+vim.g.python3_host_prog = vim.fn.expand("~") .. "/.virtualenvs/py3nvim/bin/python"
 vim.opt.inccommand = "nosplit"
 vim.opt.relativenumber = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.g.root_spec = { "cwd" }
