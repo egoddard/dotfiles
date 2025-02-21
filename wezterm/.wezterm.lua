6a365c3a6c3b8145939d846850f6972ee89f7863
Eric Goddard <3970088+egoddard@users.noreply.github.com>
2025-02-21 08:15:59 -0600

    Fix wezterm font for mac/linux

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -13,6 +13,25 @@ function scheme_for_appearance(appearance)
 	end
 end
 
+function get_font_name()
+	local os_name = ""
+	local font_name = "Fira Code"
+	if package.config:sub(1, 1) == "\\" then
+		os_name = "Windows"
+	else
+		local f = io.popen("uname -s")
+		if f then
+			os_name = f:read("*a"):gsub("\n", "")
+			f:close()
+		end
+	end
+	if os_name ~= "Darwin" then
+		font_name = "FiraCode Nerd Font"
+	end
+
+	return font_name
+end
+
 local custom_catppuccin_mocha = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 custom_catppuccin_mocha.background = "#11111b"
 
@@ -25,7 +44,9 @@ config.color_schemes = {
 	["Catppuccin Dark Macchiato"] = custom_catppuccin_macchiato,
 }
 color_scheme_dirs = { "~/.config/wezterm/colors" }
-config.font = wezterm.font_with_fallback({ "FiraCode Nerd Font", "Fira Code" })
+--config.font = wezterm.font_with_fallback({ "FiraCode Nerd Font", "Fira Code" })
+config.font = wezterm.font(get_font_name())
+config.warn_about_missing_glyphs = false
 config.font_size = 13
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.audible_bell = "Disabled"
